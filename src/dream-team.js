0b8981b617c27f27f0f01bfe99bf8b74d38acf4f
Alex080498 <sasha.kanischewa@yandex.ru>
2021-03-07 15:19:03 +0300

    feat: add dreamteam solving

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+  if (members === null) {
+    return false;
+  }
+  let array = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) == 'string') {
+      let name = members[i].trim();
+      array.push(name[0]);
+    }
+  }
+
+  let acronym = array.join(',').toUpperCase().split(',').sort().join('');
+  return acronym;
 };
