b563b43e2497116e65a1bc8d32a3b40c72513664
Alex080498 <sasha.kanischewa@yandex.ru>
2021-03-09 23:01:00 +0300

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === '--discard-next') {
+        let res = array.slice(0, i) + "," + array.slice(i + 2);
+        let newArr = res.split(',');
+        return newArr;
+    } else if (array[i] === '--discard-prev') {
+        let res = array.slice(0, i - 1) + "," + array.slice(i + 1);
+        let newArr = res.split(',');
+        return newArr;
+    } else if (array[i] === '--double-next') {
+        let res= arr.slice(0, i) + "," + array[i + 1] + "," + array.slice(i + 1);
+        let newArr = res.split(',');
+        return newArr;
+    } else if (array[i] === '--double-prev') {
+        let res = arrat.slice(0, i) + "," + array[i - 1] + "," + array.slice(i + 1);
+        let newArr = res.split(',');
+        return newArr;
+    }
+}
 };
+
